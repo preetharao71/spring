4d974a183f6077e78006cfd08a84d2be9b3a0d1c
preetharao71 <52621064+preetharao71@users.noreply.github.com>
2019-07-11 11:57:07 +0530

    Add files via upload

---
+++ JPASample/src/pojopack/Students_.java
@@ -0,0 +1,15 @@
+package pojopack;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-07-09T11:34:18.383+0530")
+@StaticMetamodel(Students.class)
+public class Students_ {
+	public static volatile SingularAttribute<Students, String> studentName;
+	public static volatile SingularAttribute<Students, Integer> studentID;
+	public static volatile SingularAttribute<Students, String> studentAddress;
+	public static volatile SingularAttribute<Students, Integer> age;
+	public static volatile SingularAttribute<Students, String> courseName;
+}
